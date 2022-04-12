d274ec0c2b8f47c001b3685e26d9e0520fd28bbb
Andro76as <andro76as@gmail.com>
2022-04-12 21:33:00 +0300

    Initial commit

---
+++ src/ru/netology/AndreyS/Main.java
@@ -0,0 +1,32 @@
+package ru.netology.AndreyS;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        int port = 80;
+        System.out.println("Server started...");
+        try {
+            ServerSocket serverSocket = new ServerSocket(port); // порт можете выбрать любой в доступном диапазоне 0-65536. Но чтобы не нарваться на уже занятый - рекомендуем использовать около 8080
+
+            Socket clientSocket = serverSocket.accept(); // ждем подключения
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+            System.out.println("Connection completed");
+
+            final String name = in.readLine();
+
+            out.println(String.format("Hi %s, your port is %d, ip address: %s", name,
+                    clientSocket.getPort(), clientSocket.getInetAddress()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
